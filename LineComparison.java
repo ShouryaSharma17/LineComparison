45cc8886a7219563197f183410d5cd0453e37e18
ShouryaSharma17 <shouryasharma17@gmail.com>
2021-04-07 22:19:33 +0530

    Comparing two lines on basis of end points

--- LineComparison.java
+++ LineComparison.java
@@ -1,41 +1,37 @@
 import java.util.*;
+
+
 public class LineComparison {
     public static void main(String args[]) {
         Scanner input = new Scanner(System.in);
-    //co-ordinates for line1
-        System.out.println("Enter value of x1 ");
-        int x1 = input.nextInt();
+        //co-ordinates line1
 
-        System.out.println("Enter value of y1");
-        int y1 = input.nextInt();
-
-        System.out.println("Enter value of x2 ");
+        System.out.println("Enter the value of x2 ");
         int x2 = input.nextInt();
 
-        System.out.println("Enter value of y2");
+        System.out.println("Enter the value of y2");
         int y2 = input.nextInt();
 
-    //co-ordinates for line2
-        System.out.println("Enter value of a1 ");
-        int a1 = input.nextInt();
+        //co-ordinates for line2
 
-        System.out.println("Enter value of b1");
-        int b1 = input.nextInt();
-
-        System.out.println("Enter value of a2 ");
+        System.out.println("Enter the value of a2 ");
         int a2 = input.nextInt();
 
-        System.out.println("Enter value of b2");
+        System.out.println("Enter the value of b2");
         int b2 = input.nextInt();
 
-        double line1_length = 0;
-        double line2_length = 0;
+        int x1 = 0;
+        int y1 = 0;
+        int a1 = 0;
+        int b1 = 0;
 
-        line1_length = Math.sqrt(Math.pow((x2 - x1),2)+ Math.pow((y2 - y1),2));
-        line2_length = Math.sqrt(Math.pow((a2 - a1),2)+ Math.pow((b2 - b1),2));
-        System.out.println("Length of line is " + line1_length+" length of line 2 is: "+line2_length);
+        double line1_length = Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
+        double line2_length = Math.sqrt(Math.pow((a2 - a1), 2) + Math.pow((b2 - b1), 2));
+        System.out.println("Length of line is " + line1_length + " length of line 2 is: " + line2_length);
 
-        System.out.println(Objects.equals(line1_length, line2_length));
+
+        System.out.println(Objects.equals(line1_length,line2_length));
     }
-}
 
+
+}
